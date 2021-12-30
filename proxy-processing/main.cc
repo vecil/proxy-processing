0c005b15bde91e42bc852d7d87812acbae75719a
vecil <69813615+vecil@users.noreply.github.com>
2021-12-30 10:22:47 +0100

    Create exec() function
    + Import WinAPI

--- proxy-processing/main.cc
+++ proxy-processing/main.cc
@@ -1,4 +1,14 @@
+#include <windows.h>
+#define WIN32_LEAN_AND_MEAN
+
 #include <iostream>
+#include <string>
+#include <string_view>
+
+std::uint32_t exec(std::string_view)
+{
+	return 0;
+}
 
 int main()
 {
