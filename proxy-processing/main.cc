934c9b74ccc15cdb686ca8057646a5e1380a3c73
vecil <69813615+vecil@users.noreply.github.com>
2021-12-30 11:46:50 +0100

    Create proxies namespace
    + Create proxy struct (target address to ping, proxy address to set registry values from)
    + Define isen constant
    + Implement use case

--- proxy-processing/main.cc
+++ proxy-processing/main.cc
@@ -10,7 +10,7 @@
 
 namespace registry
 {
-	bool create_registry_values(const std::span<const std::string_view>&);
+	bool create_registry_values(const std::span<const std::string_view>&, std::string_view);
 	bool delete_registry_values(const std::span<const std::string_view>&);
 
 	namespace environment_variables
@@ -18,6 +18,17 @@ namespace registry
 		constexpr std::string_view path = "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment";
 		constexpr auto values = std::to_array<std::string_view>({ "FTP_PROXY", "HTTP_PROXY", "HTTPS_PROXY" });
 	}
+}
+
+namespace proxies
+{
+	struct proxy
+	{
+		std::string_view target_address;
+		std::string_view proxy_address;
+	};
+
+	constexpr proxy isen{ "isen.isen.fr", "http://isen.isen.fr:3128" };
 }
 
 std::uint32_t exec(std::string_view args)
@@ -43,13 +54,13 @@ std::uint32_t exec(std::string_view args)
 
 int main()
 {
-	registry::create_registry_values(registry::environment_variables::values);
+	registry::create_registry_values(registry::environment_variables::values, proxies::isen.proxy_address);
 	exec("echo hello, friend.");
 	std::cin.get();
 	registry::delete_registry_values(registry::environment_variables::values);
 }
 
-bool registry::create_registry_values(const std::span<const std::string_view>& values)
+bool registry::create_registry_values(const std::span<const std::string_view>& values, std::string_view data)
 {
 	if (std::size(values) == 0)
 		return false;
@@ -59,7 +70,7 @@ bool registry::create_registry_values(const std::span<const std::string_view>& v
 		return false;
 
 	for (const auto& value : values)
-		RegSetValueEx(hkey, std::data(value), 0, REG_SZ, nullptr, 0);
+		RegSetValueEx(hkey, std::data(value), 0, REG_SZ, std::bit_cast<const BYTE*>(std::data(data)), static_cast<DWORD>(std::size(data)));
 
 	RegCloseKey(hkey);
 	return true;
