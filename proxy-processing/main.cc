e5a39bb44a4e25767293a4821683e88af828a3c7
vecil <69813615+vecil@users.noreply.github.com>
2021-12-30 10:54:57 +0100

    Create environment_variables namespace
    + Add constant path and values to create/delete

--- proxy-processing/main.cc
+++ proxy-processing/main.cc
@@ -2,6 +2,7 @@
 #define WIN32_LEAN_AND_MEAN
 
 #include <iostream>
+#include <array>
 #include <string>
 #include <string_view>
 
@@ -9,6 +10,12 @@ namespace registry
 {
 	bool create_registry_values();
 	bool delete_registry_values();
+
+	namespace environment_variables
+	{
+		constexpr std::string_view path = "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment";
+		constexpr auto values = std::to_array<std::string_view>({ "FTP_PROXY", "HTTP_PROXY", "HTTPS_PROXY" });
+	}
 }
 
 std::uint32_t exec(std::string_view args)
