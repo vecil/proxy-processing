f3c10134be8cf1fd2df6c6f385efa1e0cd0a1901
vecil <69813615+vecil@users.noreply.github.com>
2022-01-07 00:32:28 +0100

    Reformat code

--- proxy-processing/main.cpp
+++ proxy-processing/main.cpp
@@ -12,51 +12,49 @@
 #include <string>
 #include <string_view>
 
+using namespace std::literals;
+
 namespace registry
 {
-	bool create_registry_values(const std::span<const std::string_view>&, std::string_view);
-	bool delete_registry_values(const std::span<const std::string_view>&);
+	auto create_registry_values(const std::span<const std::string_view>&, std::string_view) -> bool;
+	auto delete_registry_values(const std::span<const std::string_view>&) -> bool;
 
 	namespace environment_variables
 	{
-		constexpr std::string_view path = "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment";
-		constexpr auto values = std::to_array<std::string_view>({ "FTP_PROXY", "HTTP_PROXY", "HTTPS_PROXY" });
+		constexpr auto path{ "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment"sv };
+		constexpr auto values{ std::to_array({ "FTP_PROXY"sv, "HTTP_PROXY"sv, "HTTPS_PROXY"sv }) };
 	}
 }
 
 namespace proxies
 {
-	std::string retrieve_proxy_address();
-	constexpr std::wstring_view website_to_ping = L"https://cloudflare.com";
+	auto retrieve_proxy_address() -> std::string;
+	constexpr auto website_to_ping{ L"https://cloudflare.com"sv };
 
 	WINHTTP_AUTOPROXY_OPTIONS autoproxy_options
 	{
-		.dwFlags = WINHTTP_AUTOPROXY_AUTO_DETECT,
-		.dwAutoDetectFlags = WINHTTP_AUTO_DETECT_TYPE_DHCP | WINHTTP_AUTO_DETECT_TYPE_DNS_A
+		.dwFlags{ WINHTTP_AUTOPROXY_AUTO_DETECT },
+		.dwAutoDetectFlags{ WINHTTP_AUTO_DETECT_TYPE_DHCP | WINHTTP_AUTO_DETECT_TYPE_DNS_A }
 	};
 	WINHTTP_PROXY_INFO proxy_info{};
 }
 
-int main()
+auto main() -> int
 {
-	const std::string proxy_address = proxies::retrieve_proxy_address();
+	const auto proxy_address{ proxies::retrieve_proxy_address() };
 
 	if (std::empty(proxy_address))
-	{
 		registry::delete_registry_values(registry::environment_variables::values);
-	}
 	else
-	{
 		registry::create_registry_values(registry::environment_variables::values, proxy_address);
-	}
 }
 
-bool registry::create_registry_values(const std::span<const std::string_view>& values, std::string_view data)
+auto registry::create_registry_values(const std::span<const std::string_view>& values, std::string_view data) -> bool
 {
 	if (std::empty(values) || std::empty(data))
 		return false;
 
-	HKEY hkey = nullptr;
+	auto hkey{ HKEY{} };
 	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, std::data(registry::environment_variables::path), 0, KEY_SET_VALUE, &hkey) != ERROR_SUCCESS)
 		return false;
 
@@ -72,7 +70,7 @@ bool registry::delete_registry_values(const std::span<const std::string_view>& v
 	if (std::empty(values))
 		return false;
 
-	HKEY hkey = nullptr;
+	auto hkey{ HKEY{} };
 	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, std::data(registry::environment_variables::path), 0, KEY_QUERY_VALUE | KEY_SET_VALUE, &hkey) != ERROR_SUCCESS)
 		return false;
 
@@ -86,12 +84,13 @@ bool registry::delete_registry_values(const std::span<const std::string_view>& v
 
 std::string proxies::retrieve_proxy_address()
 {
-	const HINTERNET winhttp_session = WinHttpOpen(nullptr, WINHTTP_ACCESS_TYPE_NO_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
+	const auto winhttp_session{ WinHttpOpen(nullptr, WINHTTP_ACCESS_TYPE_NO_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0) };
 	if (winhttp_session == nullptr)
 		return {};
 
 	if (!WinHttpGetProxyForUrl(winhttp_session, std::data(proxies::website_to_ping), &autoproxy_options, &proxy_info))
 		return {};
+	WinHttpCloseHandle(winhttp_session);
 
 	if (proxy_info.lpszProxyBypass != nullptr)
 		GlobalFree(proxy_info.lpszProxyBypass);
@@ -99,14 +98,14 @@ std::string proxies::retrieve_proxy_address()
 	if (proxy_info.lpszProxy == nullptr)
 		return {};
 
-	std::wstring wide_proxy_address{ proxy_info.lpszProxy };
+	const auto wide_proxy_address{ std::wstring{ proxy_info.lpszProxy } };
 	GlobalFree(proxy_info.lpszProxy);
 
-	const auto size = WideCharToMultiByte(CP_UTF8, 0, &wide_proxy_address.at(0), static_cast<int>(std::size(wide_proxy_address)), nullptr, 0, nullptr, nullptr);
+	const auto size{ WideCharToMultiByte(CP_UTF8, 0, &wide_proxy_address.at(0), static_cast<int>(std::size(wide_proxy_address)), nullptr, 0, nullptr, nullptr) };
 	if (size <= 0)
 		return {};
 
-	std::string proxy_address(size, 0);
+	auto proxy_address{ std::string(size, 0) };
 	WideCharToMultiByte(CP_UTF8, 0, &wide_proxy_address.at(0), static_cast<int>(std::size(wide_proxy_address)), &proxy_address.at(0), size, nullptr, nullptr);
 
 	return std::string{ "http://" + proxy_address };
